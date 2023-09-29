daf268082c3508c3e72078d9275202d5ade08ee2
karolbgm <karolyne.morgan@outlook.com>
2023-09-28 19:51:54 -0600

    displayed metric

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
-class Tamagotchi{
-    constructor(name, age=0, hunger=1, sleepiness=1, boredom=1){
-      this.name = name;
-      this.age = age;
-      this.hunger = hunger;
-      this.sleepiness = sleepiness;
-      this.boredom = boredom;
+class Tamagotchi {
+    constructor(name, age = 0, hunger = 1, sleepiness = 1, boredom = 1) {
+        this.name = name;
+        this.age = age;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom;
     }
+    feeding() {
+        return this.hunger - 1
+        checkMetrics()
+    }
+    playing() {
+        return this.boredom - 1
+        checkMetrics()
+    }
+    sleeping() {
+        return this.sleepiness - 1
+        checkMetrics()
+    }
+    aging() {
+        return this.age + 1
+        checkMetrics()
+    }
+    hunger() {
+        return this.hunger + 1
+        checkMetrics()
+    }
+    boredom() {
+        return this.boredom + 1
+        checkMetrics()
+    }
+    sleepiness() {
+        return this.sleepiness + 1
+        checkMetrics()
+    }
+
 }
 
 const myTamagotchi = new Tamagotchi("bubble")
+
+function checkMetrics() {
+    if ((this.hunger === 10) ||
+        (this.boredom === 10) ||
+        (this.sleepiness === 10)) {
+        return 'Your pet died!'
+    }
+}
