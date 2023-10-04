d47e9f5ff721f25c24cab1b757c82b59e0a73300
karolbgm <karolyne.morgan@outlook.com>
2023-10-03 19:32:01 -0600

    updated sleepiness bar and event listener, created new intervals, animated my pet

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// class Tamagotchi {
+//class Tamagotchi {
 //     constructor(name, age = 0, hunger = 1, sleepiness = 1, boredom = 1) {
 //         this.name = name;
 //         this.age = age;
@@ -8,195 +8,126 @@
 //     }
 //     feeding() {
 //         this.hunger = this.hunger - 1
-        
 //     }
 //     playing() {
 //         this.boredom = this.boredom - 1
-        
 //     }
 //     sleeping() {
 //         this.sleepiness = this.sleepiness - 1
-        
 //     }
 //     aging() {
 //         this.age = this.age + 1
-        
 //     }
 //     getHungry() {
 //         this.hunger = this.hunger + 1
-        
 //     }
 //     getBored() {
 //         this.boredom = this.boredom + 1
-        
 //     }
 //     getSleepy() {
 //         this.sleepiness = this.sleepiness + 1
-        
 //     }
-
 // }
-
 // const myTamagotchi = new Tamagotchi("bubble")
-
 const tamagotchi = {
     age: 0
 }
-
 const progressBar1 = document.querySelector('#hunger');
 const progressBar2 = document.querySelector('#sleep');
 const progressBar3 = document.querySelector('#bored');
-
-const ageInterval = setInterval(function() {
+const ageInterval = setInterval(function () {
     tamagotchi.age++;
     const age = document.querySelector('#age');
-    const pet = document.querySelector('#petImg');
+    const pet = document.querySelector('.petImg');
     age.innerHTML = `Age: ${tamagotchi.age}`;
     if (tamagotchi.age >= 4) {
         pet.src = 'img/gif3.gif';
+        pet.classList.add('adult')
     } else if (tamagotchi.age >= 2) {
         pet.src = 'img/gif2.gif';
+        pet.classList.add('teen')
     }
-    // aging();
 }, 3000);
-
-// function aging(){
-//     const pet = document.querySelector('#a')
-//     if (tamagotchi.age >= 2){
-//         pet.src = 'img/gif2.gif'
-//     } else if (tamagotchi.age >= 4){
-//         pet.src = 'img/gif3.gif'
-//     }
-// }
-
-const interval1 = setInterval(function(){
+const hungerInterval = setInterval(function () {
     progressBar1.value++;
+}, 2200)
+let awakeInterval = setInterval(function () {
     progressBar2.value++;
 }, 2200)
-
-const interval2 = setInterval(function(){
-    progressBar3.value++;;
-  }, 2600)
-
-  function checkMetrics() {
-    if((progressBar1.value === 10) ||
-    (progressBar2.value === 10) ||
-    (progressBar3.value ===10)){
+const boredInterval = setInterval(function () {
+    progressBar3.value++;
+}, 2600)
+function checkMetrics() {
+    if ((progressBar1.value === 10) ||
+        (progressBar2.value === 10) ||
+        (progressBar3.value === 10)) {
         feedBtn.disabled = true;
         lightsBtn.disabled = true;
         playBtn.disabled = true;
-        alert('Your pet died!')
-        clearInterval(interval1)
-        clearInterval(interval2)
+        clearInterval(hungerInterval)
+        clearInterval(awakeInterval)
+        clearInterval(boredInterval)
         clearInterval(ageInterval)
+        alert('Your pet died!')
     }
 }
-
+const html = document.querySelector('html');
+let sleepInterval; // Declare sleepInterval outside of the if block
+function runSleep() {
+    if (html.classList.contains('image-overlay')) {
+        feedBtn.disabled = true;
+        playBtn.disabled = true;
+        clearInterval(awakeInterval);
+        sleepInterval = setInterval(function () {
+            progressBar2.value--;
+            if (progressBar2.value === 0) {
+                clearInterval(sleepInterval);
+            }
+        }, 1000);
+    } else {
+        clearInterval(sleepInterval); // Clear the interval if it exists
+        startAwakeInterval();
+        feedBtn.disabled = false;
+        playBtn.disabled = false;
+    }
+}
+function startAwakeInterval() {
+    clearInterval(awakeInterval);
+    awakeInterval = setInterval(function () {
+        progressBar2.value++;
+    }, 2200);
+}
 const feedBtn = document.querySelector('#feed');
-feedBtn.addEventListener('click', () => { 
-const progressBar = document.querySelector('#hunger');
-progressBar.value--;
-checkMetrics()
-// Restore the original background image
-const body = document.body;
-  body.style.backgroundImage = 'url(img/newimg.jpg)';
+feedBtn.addEventListener('click', () => {
+    progressBar1.value--;
+    checkMetrics()
+    // Restore the original background image
+    const body = document.body;
+    body.style.backgroundImage = 'url(img/newimg.jpg)';
 });
-
 const lightsBtn = document.querySelector('#lightsOff');
-lightsBtn.addEventListener('click', () => { 
-    const button1 = document.querySelector('#feed')
-    const button2 = document.querySelector('#play')
-    button1.disabled = true;
-    button2.disabled = true;
-const html = document.querySelector('html')
-html.classList.toggle('image-overlay')    
-const progressBar = document.querySelector('#sleep');
-clearInterval(interval1)
-progressBar.value--;
-checkMetrics()
-// Restore the original background image
-const body = document.body;
-  body.style.backgroundImage = 'url(img/newimg.jpg)';
+lightsBtn.addEventListener('click', () => {
+    // const html = document.querySelector('html');
+    html.classList.toggle('image-overlay');
+    // Restore the original background image
+    const body = document.body;
+    body.style.backgroundImage = 'url(img/newimg.jpg)';
+    runSleep()
+    checkMetrics()
+    // Restore the original background image
+    // const body = document.body;
+    // body.style.backgroundImage = 'url(img/newimg.jpg)';
 });
-
-// const lightsBtn = document.querySelector('#lightsOff');
-// lightsBtn.addEventListener('click', () => { 
-// const img = document.body.style.backgroundImage
-// img.opacity = 0.5;
-
-// progressBar.value--;
-// checkMetrics()
-// // Restore the original background image
-//   body.style.backgroundImage = 'url(img/newimg.jpg)';
-// });
-
 const playBtn = document.querySelector('#play');
-playBtn.addEventListener('click', () => { 
-const body = document.body
-body.style.backgroundImage = 'url(img/outsidef.jpg)' 
-const progressBar = document.querySelector('#bored');
-progressBar.value--;
-checkMetrics()
+playBtn.addEventListener('click', () => {
+    const body = document.body
+    body.style.backgroundImage = 'url(img/outsidef.jpg)'
+    progressBar3.value--;
+    checkMetrics()
 });
-
-// const inputName = document.querySelector('#petName')
-
-//  document.getElementById("startGame").addEventListener("click", function (event) {
-//     const tamaName = inputName.value;
-//     localStorage.setItem('tamaName', tamaName)
-//     // Redirect to the main page (replace 'main.html' with your main page's URL)
-//     window.location.href = "main.html";
-// });
-
-// if (tamaName){
-//     const tamagotchiName = document.getElementById("tamagotchiName");
-//     const petName = localStorage.getItem('tamaName')
-//     tamagotchiName.innerHTML = petName;
-// } 
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Your code here
-//     document.getElementById("startGame").addEventListener("click", function (event) {
-//         const inputName = document.querySelector('#petName');
-//         const tamaName = inputName.value;
-        
-//         if (tamaName) {
-//             // Save tamaName to localStorage
-//             localStorage.setItem('tamaName', tamaName);
-    
-//             // Redirect to the main page (replace 'main.html' with your main page's URL)
-//             window.location.href = "main.html";
-//         } else {
-//             alert("Please enter a Tamagotchi name.");
-//         }
-//     });
-    
-//     // Check tamaName and update tamagotchiName on the main page
-//     const tamaName = localStorage.getItem('tamaName');
-//     if (tamaName) {
-//         const tamagotchiName = document.getElementById("tamagotchiName");
-//         tamagotchiName.innerHTML = tamaName;
-//     }
-// });
-// document.getElementById("startGame").addEventListener("click", function (event) {
-//     const inputName = document.querySelector('#petName');
-//     const tamaName = inputName.value;
-    
-//     if (tamaName) {
-//         // Save tamaName to localStorage
-//         localStorage.setItem('tamaName', tamaName);
-
-//         // Redirect to the main page (replace 'main.html' with your main page's URL)
-//         window.location.href = "main.html";
-//     } else {
-//         alert("Please enter a Tamagotchi name.");
-//     }
-// });
-
 // // Check tamaName and update tamagotchiName on the main page
-// const tamaName = localStorage.getItem('tamaName');
-// if (tamaName) {
-//     const tamagotchiName = document.getElementById("tamagotchiName");
-//     tamagotchiName.innerHTML = tamaName;
-// }
-
+const petName = localStorage.getItem('tamagotchiName');
+if (petName) {
+    const tamagotchi = document.getElementById("tamagotchi");
+    tamagotchi.innerHTML = petName;
+}
